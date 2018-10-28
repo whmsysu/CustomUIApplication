4c326c976941de0ca89c6b397962e0adb2d5881c
Haomin Wu <wuhaominsysu@gmail.com>
2018-10-28 23:35:21 +0800

    Init commit

---
+++ app/src/main/java/com/application/haominwu/customuiapplication/CustomUIApplication.java
@@ -0,0 +1,14 @@
+package com.application.haominwu.customuiapplication;
+
+import android.app.Application;
+
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
+public class CustomUIApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Logger.addLogAdapter(new AndroidLogAdapter());
+    }
+}
---
+++ app/src/main/java/com/application/haominwu/customuiapplication/MoveView.java
@@ -0,0 +1,55 @@
+package com.application.haominwu.customuiapplication;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+
+public class MoveView extends android.support.v7.widget.AppCompatTextView {
+
+    private double initRawX;
+    private double initRawY;
+
+    public MoveView(Context context) {
+        super(context);
+    }
+
+    public MoveView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MoveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                initRawX = event.getRawX();
+                initRawY = event.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveViewByLayout(event.getRawX(), event.getRawY());
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return true;
+    }
+
+    private void moveViewByLayout(double rawX, double rawY) {
+        int dx = (int)(rawX - initRawX);
+        int dy = (int)(rawY - initRawY);
+        int left = getLeft() + dx;
+        int top = getTop() + dy;
+        int right = left + getWidth();
+        int bottom = top + getHeight();
+        layout(left, top, right, bottom);
+        initRawX = rawX;
+        initRawY = rawY;
+    }
+
+}
